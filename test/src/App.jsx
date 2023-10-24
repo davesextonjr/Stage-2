03140f8fcb59b39786f35db29b95d0d65eaa4123
David Sexton Jr <davesextonjr@gmail.com>
2023-10-24 15:13:21 -0400

    Init Commit

---
+++ test/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(import.meta.env.VITE_URL, import.meta.env.VITE_ANON_KEY)
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
+
+  return (
+    <ul>
+      {countries.map((country) => (
+        <li key={country.name}>{country.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
